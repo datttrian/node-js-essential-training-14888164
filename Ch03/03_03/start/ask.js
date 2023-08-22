494bfc7f18c1441e0fd7962c0eab02ddab4bf6cd
datttrian <datttrian@gmail.com>
2023-08-22 20:58:32 +0000

    Using readline

--- Ch03/03_03/start/ask.js
+++ Ch03/03_03/start/ask.js
@@ -1,9 +1,31 @@
 const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-rl.question("How do you like Node?", (answer) => {
-  console.log(`Your answer: ${answer}`);
+const questions = [
+  "What is your name?",
+  "Where do you live?",
+  "What are you going to do with Node.js?",
+];
+
+function collectAnswer(questions, done) {
+  const answers = [];
+  const questionAnswered = (answer) => {
+    answers.push(answer.trim());
+    if (answers.length < questions.length) {
+      rl.question(questions[answers.length], questionAnswered);
+    } else {
+      return done(answers);
+    }
+  };
+
+  rl.question(questions[0], questionAnswered);
+}
+
+collectAnswer(questions, (answers) => {
+  console.log("Thank you for your answers!");
+  console.log(answers);
+  process.exit();
 });
